ee633c1a52c031135d90802f6e0f94ac424241bb
Murilo de Oliveira <murilo.o.d.f@gmail.com>
2025-08-19 21:59:22 -0300

    fix: adding old functions back

--- main.py
+++ main.py
@@ -26,9 +26,14 @@ def get_github_user(username: str) -> dict:
         "following": data.get("following"),
         "url": data.get("html_url"),
     }
- 
+
+def get_ip():
+    """Retorna o IP público usando a API ipify"""
+    url = "https://api.ipify.org?format=json"
+    resp = httpx.get(url, timeout=5)
+    return resp.json()["ip"]
  
 if __name__ == "__main__":
     # Exemplo de uso: consultar o próprio usuário do GitHub
     user = get_github_user("octocat")
-    print("Usuário encontrado:", user)
+    print(f"Usuário encontrado: {user} | Seu IP público é: {get_ip()}")
