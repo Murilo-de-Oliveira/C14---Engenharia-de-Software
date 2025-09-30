0f33df9b06e33feff44b2ec9a820acca9c7f1853
Murilo de Oliveira <murilo.o.d.f@gmail.com>
2025-09-30 08:53:46 -0300

    feature:  Adicionando envio de email ao finalizar pipeline

---
+++ scripts/shell.sh
@@ -0,0 +1,6 @@
+cd Aula-GitHub-Actions
+ls
+echo "Rodou o pipeline"
+pwd
+sudo apt-get install mailutils
+echo "Mandando e-mail com mail do linux" | mail -s "a subject" emiliocfigueiredo@yahoo.com.br
