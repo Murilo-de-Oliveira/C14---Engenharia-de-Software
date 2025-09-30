dd6065936fc93bdff51520d8be399d235ce919b4
Murilo de Oliveira <murilo.o.d.f@gmail.com>
2025-09-30 09:47:54 -0300

    fix: Adicionando variável de secrets

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -3,4 +3,4 @@ ls
 echo "Rodou o pipeline"
 pwd
 sudo apt-get install mailutils
-echo "Mandando e-mail com mail do linux" | mail -s "a subject" emiliocfigueiredo@yahoo.com.br
+echo "Mandando e-mail com mail do linux" | mail -s "a subject" "$DEST_EMAIL"
